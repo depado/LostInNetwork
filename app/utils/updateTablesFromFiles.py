5acf67f3d67f6d2d824037f2576c7d81f984f1a0
hiziv <tor@lin.lan>
2015-03-26 17:57:31 +0100

    updatefiles

--- app/utils/updateTablesFromFiles.py
+++ app/utils/updateTablesFromFiles.py
@@ -12,23 +12,33 @@ def main():
     for device in Device.query.all():
         run_file="data/" + device.name + "-run.txt"
         route_file="data/" + device.name + "-route.txt"
-        run_configuration=Configuration(path=run_file,device=device)
         route_configuration=Configuration(path=route_file,device=device)
-        run_configuration.save()
-        route_configuration.save()
-        for lines in open(run_file):
+        try:
+            run_configuration=Configuration(path=run_file,device=device)
+            run_configuration.save()
+            route_configuration.save()
+        except:
+            continue 
+            #run_configuration.delete()
+            #run_configuration.save()
+            #route_configuration.delete()
+            #route_configuration.save()
+    for conf in Configuration.query.all():            
+        for lines in open(conf.path):
             lines=lines.strip()
             version_Search=re.search('(?i)version\s(.*)',lines)
             if version_Search:
                 run_version=version_Search.group(1)
-                run_configurationValue=ConfigurationValues(version=run_version,configuration_id=configurationvalues.id)
-                run_configurationValue.save()
-#                print ("version :"+ version)
+                run_configurationValue=ConfigurationValues(version=run_version,configuration_id=conf.id)
+                try:
+                    run_configurationValue.save()
+                except:
+                    continue
 #            interface_Search=re.search('(?i)interface\s(.*)',lines)
 #            if interface_Search:
 #                interface=interface_Search.group(1)
 #                print ("interface : "+ interface)
 
 if __name__ == '__main__':
-                    main()
+    main()
 
