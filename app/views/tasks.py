2897c30000abe112f300f6a28022d4354ebac0f8
huggy <huggy@asu.home.kich.land>
2015-03-27 15:49:01 +0100

    update view/tasks.py / add psendcomand

--- app/views/tasks.py
+++ app/views/tasks.py
@@ -6,7 +6,7 @@ from flask_login import login_required
 from app.utils import down_cve, read_cve, update_cve
 
 from app import app
-
+from app.utils.psendcommand import mainsendcommand
 
 @app.route('/tasks', methods=['GET', 'POST'])
 @login_required
@@ -17,6 +17,10 @@ def tasks():
             data = read_cve()
             update_cve(data)
             flash("Update finished", "info")
-            return redirect(url_for('updates'))
+            return redirect(url_for('tasks'))
+        if request.form['psend-btn'] == 'psend':
+            mainsendcommand()
+            flash("psendcommand finished", "info")
+            return redirect(url_for('tasks'))
 
     return render_template("tasks.html", active_page="tasks")
